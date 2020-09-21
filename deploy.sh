f57097863fc947b83b2c383104c2670add5a2993
Joshua Skootsky <joshua.skootsky@gmail.com>
2020-09-21 11:57:30 -0400

    main branch deploy updated

--- deploy.sh
+++ deploy.sh
@@ -3,32 +3,25 @@
 # if command fails, whole script stops
 set -e
 
-function cleanup_at_exit {
-    # return to main branch
-    git checkout main
+# function cleanup_at_exit {
+#    # return to main branch
+# }
+# trap cleanup_at_exit EXIT
 
-    #remove the deploy branch
-    git branch -D deploy
-}
-trap cleanup_at_exit EXIT
-
-git checkout -b deploy
-
+# Subtree method here: https://gohugo.io/hosting-and-deployment/hosting-on-github/
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # build
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-git add -f public/
 
 # Commit changes.
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
-git commit -m "$msg"
+
+cd public && git add --all && git commit -m "$msg" && cd ..
 
 
 # Push source and build repos.
-git push github deploy
-
+git push github gh-pages
