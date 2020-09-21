e4f26ca265fc07e0e74a129bbe2ade566f4f4b9b
Joshua Skootsky <joshua.skootsky@gmail.com>
2020-09-21 12:26:19 -0400

    deploy  script to github pages works

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,22 @@ set -e
 # }
 # trap cleanup_at_exit EXIT
 
-# Subtree method here: https://gohugo.io/hosting-and-deployment/hosting-on-github/
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# Subtree method here: https://gohugo.io/hosting-and-deplooyment/hosting-on-github/
 
-# build
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public github/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Building site with Hugo..."
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Commit changes.
@@ -20,8 +32,10 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
+echo "Updating gh-pages branch with message {$msg}"
 cd public && git add --all && git commit -m "$msg" && cd ..
 
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Push source and build repos.
 git push github gh-pages
